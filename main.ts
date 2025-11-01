c5dad3684049197480564a31c895fe7f6b11369a
Adam Cohen-Rose <github.com@cohen-rose.org>
2025-11-01 10:52:12 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,25 +18,53 @@ radio.onReceivedNumber(function (receivedNumber) {
                     `)
             }
         }
-    } else {
+        if (receivedNumber == 2) {
+            for (let index = 0; index < 4; index++) {
+                basic.showLeds(`
+                    . . . . .
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    . . # . .
+                    . . . . .
+                    `)
+            }
+        }
         basic.showLeds(`
             . # # # .
-            # # # # #
-            # . . # #
-            # . . . #
+            # # . . #
+            # # # . #
+            # # . . #
             . # # # .
             `)
     }
 })
 input.onButtonPressed(Button.A, function () {
     On = 1
+    basic.showLeds(`
+        . # # # .
+        # # . . #
+        # # # . #
+        # # . . #
+        . # # # .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     On = 0
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
 let On = 0
 radio.setGroup(51)
 On = 0
-basic.forever(function () {
-	
-})
